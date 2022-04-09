fbf77158dd4c4fa756f973f8d729964364d87e52
DESKTOP-ICDQQAM\shaya <fck27.ohsagami.shun@ezweb.ne.jp>
2022-04-09 11:29:20 +0900

    速水　作成　プロジェクト作成

---
+++ abuse/src/test/java/com/snd/abuse/AbuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snd.abuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
