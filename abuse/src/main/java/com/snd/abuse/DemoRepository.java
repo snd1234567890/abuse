564f143418bbf9b01571d1b2af838a40638c6d63
snd1234567890 <sssnnnddd1234567890@gmail.com>
2022-05-22 12:10:33 +0900

    不要ファイル削除

--- abuse/src/main/java/com/snd/abuse/DemoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.snd.abuse;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface DemoRepository extends JpaRepository <demoEntity, Long> {
-}
